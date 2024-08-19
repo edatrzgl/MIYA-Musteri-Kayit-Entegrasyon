8c601dc1bd0169e7a02b9b2def23b5935a2406e4
ElberBildik <elber4568521@gmail.com>
2024-08-19 09:53:44 +0300

    Uygulama Iconu eklendi

--- Tasarim1/Properties/AssemblyInfo.cs
+++ Tasarim1/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Tasarim1")]
+[assembly: AssemblyProduct("ExcelToPanorama")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
